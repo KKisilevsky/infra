112bed02646b5293ccc58646c1774d83e8d94b17
Konstantin Kisilevskiy <xlonelysoul94@gmail.com>
2020-07-28 09:20:47 +0200

    Add terraform aks

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -8,3 +8,16 @@ provider "azurerm" {
 
   features {}
 }
+
+module "aks" {
+  source              = "../modules/aks"
+  client_id           = var.client_id
+  client_secret       = var.client_secret
+  node_count          = var.node_count
+  vm_size             = var.vm_size
+  ssh_public_key      = var.ssh_public_key
+  dns_prefix          = var.dns_prefix
+  cluster_name        = var.cluster_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+}
--- terraform/stage/variables.tf
+++ terraform/stage/variables.tf
@@ -14,3 +14,30 @@ variable "tenant_id" {
   description = "tenant"
 }
 
+variable "node_count" {
+  description = "tenant"
+}
+
+variable "ssh_public_key" {
+  description = "tenant"
+}
+
+variable "dns_prefix" {
+  description = "tenant"
+}
+
+variable cluster_name {
+  description = "tenant"
+}
+
+variable vm_size {
+  description = "tenant"
+}
+
+variable resource_group_name {
+  description = "tenant"
+}
+
+variable location {
+  description = "tenant"
+}
