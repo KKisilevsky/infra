218489b5b5a1fa8510508319526734df520ff75c
Konstantin Kisilevskiy <xlonelysoul94@gmail.com>
2020-08-26 16:53:35 +0200

    FQDN for nginx

---
+++ application-config/base/fqdn.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Public IP address of your ingress controller
+IP=52.174.194.67
+
+# Name to associate with public IP address
+DNSNAME="aks-ingress-platform"
+
+# Get the resource-id of the public ip
+PUBLICIPID=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$IP')].[id]" --output tsv)
+
+# Update public ip address with DNS name
+az network public-ip update --ids $PUBLICIPID --dns-name $DNSNAME
+
+# Display the FQDN
+az network public-ip show --ids $PUBLICIPID --query "[dnsSettings.fqdn]" --output tsv
